440b051e000c21c2e1670ed1da60940861fecae6
shreyanshjha <shreyanshjha22@gmail.com>
2023-11-13 01:48:08 +0530

    initial commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,64 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("SSJ");
+console.log(23);
+
+// Legal
+let firstName = "Shreyansh";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_malinda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1245;
+//Valid naming convention
+let myFirstJob = "Programmer";
+let myCurrentJob = "SDE";
+//invalid naming 
+let job1 = "Programmer";
+let job2 = "Associated"
+
+// Invalid in JavaScript
+//let jonas&malinda = "Jonas";
+//let new = "newData" //new is the keyword in javascript
+*/
+
+/*
+let jamesData = 'Data 123';
+console.log(typeof jamesData);
+
+let numberType = 23;
+console.log(typeof numberType);
+
+// Dynamic Typing
+numberType = 'Dynamic Type';
+console.log(typeof numberType);
+jamesData = true;
+console.log(typeof jamesData);
+
+let year;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// let, const and var
+
+let age = 42;
+age = 42;
+
+const birthYear = 1995;
+//birthYear = 1990; //Error: Can't re-assign the value to const variable
+//const job; 
+
+var program = "programmer";
+program = "Coder";
+
+lastName = "Jha";
+console.log(lastName);
