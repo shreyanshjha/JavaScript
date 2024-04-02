27e3a8f28b05f4f75363956c4fadffa81dc0b161
shreyanshjha <shreyanshjha22@gmail.com>
2024-04-02 08:58:42 +0530

    array, objects and loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -238,3 +238,141 @@ TEST DATA: 125, 555, and 44.
 // const totals = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]];
 
 // console.log(totals);
+
+/* %%%%%% Objects %%%%%% */
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtman',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+// Good example that why [] is use for retriving the data from object
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+// if(jonas[interestedIn]){
+//     console.log(jonas[interestedIn]);
+// } else {
+//     console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+// }
+
+jonas.location = 'Vadodara';
+jonas['twitter'] = '@shreyanshjha';
+console.log(jonas);
+
+// Challenge
+// Jonas has 3 friends, and his best friend is called Michael
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+const shreyansh = {
+    firstName: 'Shreyansh',
+    lastName: 'Jha',
+    birthYear:  1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+    // calcAge: function (birthYear) {
+    //     return 2037 - birthYear;
+    // }
+    // calcAge: function () {
+    //     return 2037 - this.birthYear;
+    // }
+    calcAge: function () {
+        this.age = 2037 - this.birthYear;
+        return this.age;
+    },
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+};
+
+console.log(shreyansh.calcAge());
+
+console.log(shreyansh.age);
+console.log(shreyansh.age);
+console.log(shreyansh.age);
+
+// Challenge
+// "Shreyansh is a 46-year old teacher, and he has a/no 
+//  driver's license"
+
+console.log(shreyansh.getSummary());
+
+/*vCHALLENGE #3
+Let's go back to Mark and John comparing their BMIs!
+
+This time, let's use objects to implement the calculations! 
+Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your tasks:
+
+1: For each of them, create an object with properties for their full name, mass, and height 
+(Mark Miller and John Smith). Name these objects as mark and john, and their properties 
+exactly as fullName, mass and height.
+
+2: Create a calcBMI method on each object to calculate the BMI (the same method on both objects). 
+Assign the BMI value to a property called bmi (lowercase), and also return it from the method.
+
+3: Log to the console who has the higher BMI, together with the full name and the respective 
+BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+*/
+/* Write your code below. Good luck! 🙂 */
+
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     }
+// }
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height * this.height);
+//         return this.bmi;
+//     }
+// }
+
+// mark.calcBMI();
+// john.calcBMI();
+
+// if(mark.bmi > john.bmi) {
+//     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`);
+// } else if (john.bmi > mark.bmi) {
+//     console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${mark.fullName}'s (${mark.bmi})`);
+// }
+
+// ***** Iteration: The For Loop ***************///
+// for loop keeps running while condition is TRUE
+// for(let resp = 1; resp <= 10; i++){
+//     console.log(`Lifting weights repetition ${i}`);
+// }
+const jonasArray = [
+    'Jonas',
+    'Schmedtman',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+]; 
+
+//console.log(jonasArray[0]);
+//console.log(jonasArray[1]);
+//...
+//console.log(jonasArray[4]);
+//jonasArray[5] does NOT exist;
+for(let i = 0;i < 5; i++) {
+    console.log(jonasArray[i]);
+}
