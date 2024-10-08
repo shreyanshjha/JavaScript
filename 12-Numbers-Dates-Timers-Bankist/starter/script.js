ca02169068665b650f50fb5ddfa6476d72cb9da8
shreyanshjha <shreyanshjha22@gmail.com>
2024-10-08 10:18:58 +0530

    numbers and date

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.round(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -251,3 +251,65 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2); // Output by javascript will not be perfect
+console.log(0.1 + 0.2 === 0.3); // Output by javascript will not be perfect
+
+console.log(Number('23'));
+// Type coercion
+console.log(+'23');
+
+// Parsing (parseInt only works when first char start with number)
+console.log(Number.parseInt('30px'));
+console.log(Number.parseInt('e30px')); 
+
+
+console.log(Number.parseFloat('    2.34rem   ')); 
+console.log(Number.parseFloat('  3.1rem   ')); 
+
+// checking if value is number
+console.log(Number.isFinite(20)); // true
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20x')); // false
+console.log(Number.isFinite(20 / 0)); // false
+console.log(Number.isFinite(11 /     2)); // true
+
+console.log(Number.isInteger(20));
+console.log(Number.isInteger(299.923));
+
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1/2));
+console.log(8 ** (1/3));// only way get cubic number
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, 23, 11, 2, -1));
+
+console.log(Math.min(89, 3, 6, '11', 2, -1));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+function randomInit(min, max) {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+}
+console.log(randomInit(10, 20));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+console.log(Math.trunc(-23.9));
+console.log(Math.floor(-23.9));
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // toFixed will return a string
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(Number((2.345).toFixed(2)));
